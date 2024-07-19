5d589bc4493642d0e1232e5f711e04f37cae6417
Shivani Chauhan <shivanichauhandev.@gmail.com>
2024-07-19 20:39:09 +0530

    added location and service in vendor model

--- app/model/entertainment.py
+++ app/model/entertainment.py
@@ -5,14 +5,12 @@ from sqlalchemy.exc import IntegrityError
 class Entertainment(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    contact_information = db.Column(db.String(100), nullable=False)
+    email_id=db.Column(db.String(50),unique=True)
     experience = db.Column(db.Integer, nullable=False)
     top_picks = db.Column(db.Boolean, default=False)
     price = db.Column(db.Float, nullable=False)
     location = db.Column(db.String(100), nullable=False)
     reviews = db.Column(db.Float, default=0.0)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
     availability_status = db.Column(db.Boolean, nullable=False)
 
     # bookings = db.relationship("Booking",backref="entertainment",lazy=True,)
@@ -21,13 +19,11 @@ class Entertainment(db.Model):
         return {
             "id":self.id,
             "name":self.name,
-            "contact_information":self.contact_information,
+            "email_id":self.email_id,
             "experience":self.experience,
             "top_picks":self.top_picks,
             "price":self.price,
             "location":self.location,
             "reviews":self.reviews,
-            'latitude': self.latitude,
-            'longitude': self.longitude,
             "availability_status":self.availability_status
         }
